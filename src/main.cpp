d7e61c57379a6901e3c07d10a6f903459910918c
Kürti Zoltán <kuzo1998@gmail.com>
2021-10-22 00:52:59 +0200

    Angle correction

--- src/main.cpp
+++ src/main.cpp
@@ -60,13 +60,46 @@ RGB skyColorNoCorrection(const State& state)
     return ret;
 }
 
+RGB skyColor(const State& state)
+{
+    f32 r = state.position[1];
+    f32 ctheta = std::cos(state.position[2]);
+    f32 stheta = std::sin(state.position[2]);
+    f32 sphi = std::sin(state.position[3]);
+    f32 cphi = std::cos(state.position[3]);
+    f32 dirx = state.velocity[1] / std::sqrt(1 - 2 * M / r) * stheta * cphi + state.velocity[2] * r * ctheta * cphi - state.velocity[3] * r * stheta * sphi;
+    f32 diry = state.velocity[1] / std::sqrt(1 - 2 * M / r) * stheta * sphi + state.velocity[2] * r * ctheta * sphi + state.velocity[3] * r * stheta * cphi;
+    f32 dirz = state.velocity[1] / std::sqrt(1 - 2 * M / r) * ctheta - state.velocity[2] * r * stheta;
+    f32 theta = std::atan2(std::sqrt(dirx * dirx + diry * diry), dirz);
+    f32 phi = std::atan2(diry, dirx);
+    
+    f32 scale = 6 / PI;
+    
+    bool n1 = (theta * scale - std::floor(theta * scale)) < 0.5;
+    bool n2 = (phi * scale - std::floor(phi * scale)) < 0.5;
+    RGB ret;
+    if (n1 ^ n2)
+    {
+        ret.r = 0;
+        ret.g = 0;
+        ret.b = 255;
+    }
+    else
+    {
+        ret.r = 0;
+        ret.g = 0;
+        ret.b = 127;
+    }
+    return ret;
+}
+
 int main()
 {
     Schwarzschild spacetime(M);
     Objects objects;
     objects.add(eventHorizon, eventHorizonColor);
-    objects.add(skySphere, skyColorNoCorrection);
-    ReferenceFrame referenceFrame(vec4(0, 20 * 2 * M, 3.14/2, 0), vec4(1, 0, 0, 0), vec4(0, -1, 0, 0), vec4(0, 0, -1, 0));
-	ImageRenderer renderer(spacetime, objects, referenceFrame, 1024, 1024, 3.141592/2);
+    objects.add(skySphere, skyColor);
+    ReferenceFrame referenceFrame(vec4(0, 1.5 * 2 * M, PI/2, 0), vec4(1, 0, 0, 0), vec4(0, 0, 0, 1), vec4(0, 0, -1, 0));
+	ImageRenderer renderer(spacetime, objects, referenceFrame, 1024, 1024, 70 * PI / 180);
     renderer.traceRays("../pic/test.tif");
 }
